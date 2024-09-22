86a8eccb3fb7c1c7739dedcffe8d7afd440fd501
Shreyam Saha <sahashreyam123@gmail.com>
2024-09-22 12:12:53 -0400

    Adding main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import twitterLogo from './assets/twitter.svg'
+import githubLogo from './assets/github.svg'
+import linkedinLogo from './assets/linkedin.svg'
+import profileImage from './assets/profile.jpg'
+import {Tooltip as ReactTooltip} from 'react-tooltip'
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <header className="header">
+      <Navbar />
+    </header>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+         <img src={profileImage} alt="Description of image" className="circular-image" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Shreyam Saha</h1>
+      <p className="profile-desc">Hello, I am Shreyam Saha, a software developer with over 3 years of experience. 
+        Currently working @ Amdocs, utilizing my skills in React, Kubernetes, Terraform, Docker, ELK stack, and Grafana. 
+        I am also Microsoft Azure certified. 
+        My passion for coding drives me to continuously learn and upskill, staying ahead in the tech world. 
+        I love exploring new technologies and creating scalable, efficient solutions.
       </p>
+      <div className="icon-bar">
+        <a className="social-links" data-tooltip-id="my-custom-tooltip" data-tooltip-content="Twitter" href="https://x.com/coder_panda_" target="_blank">
+          <img  src={twitterLogo} className="icon" alt="Twitter Logo"/>
+        </a>
+        <a className="social-links" data-tooltip-id="my-custom-tooltip" data-tooltip-content="LinkedIn" href="https://www.linkedin.com/in/shreyam-saha/" target="_blank">
+          <img src={linkedinLogo} className="icon" alt="LinkedIn Logo"/>
+        </a>
+        <a className="social-links" data-tooltip-id="my-custom-tooltip" data-tooltip-content="Github" href="https://github.com/Shreyam-Saha" target="_blank">
+          <img src={githubLogo} className="icon" alt="Github Logo"/>        
+        </a>
+        <ReactTooltip className="custom-tooltip" id="my-custom-tooltip" place="bottom"/>
+      </div>
     </>
   )
 }
