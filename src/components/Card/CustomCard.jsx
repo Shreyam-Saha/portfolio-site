501fb4bf2e307451347e58925c71fed9b70d7fe7
Shreyam Saha <sahashreyam123@gmail.com>
2024-09-24 06:11:52 -0400

    Fixed some UI mistakes

--- src/components/Card/CustomCard.jsx
+++ src/components/Card/CustomCard.jsx
@@ -1,54 +1,61 @@
-
 import { Card } from "antd";
-import {LinkOutlined,CheckCircleOutlined,HourglassOutlined} from '@ant-design/icons';
-import './CustomCard.css'
+import {
+  LinkOutlined,
+  CheckCircleOutlined,
+  HourglassOutlined,
+} from "@ant-design/icons";
+import "./CustomCard.css";
 
 // eslint-disable-next-line react/prop-types
-function CustomCard({title,description,img,link,status,loading}){
-
+function CustomCard({ title, description, img, link, status, loading }) {
   const handleCardClick = (link) => {
-    if(link){
-        window.open(link, '_blank');
+    if (link) {
+      window.open(link, "_blank");
     }
-     // Open the link in a new tab
+    // Open the link in a new tab
   };
 
   return (
     <Card
-    className="custom-card-container"
-      loading={loading}  // Show skeleton while loading
+      className="custom-card-container"
+      loading={loading} // Show skeleton while loading
       hoverable
-      style={{ width: '100%',minWidth:250,minHeight:300 }}
+      style={{ width: "100%", minWidth: 250, minHeight: 300 }}
       cover={
-        !loading && (<img
-        className="card-img"
-          alt="example"
-          src= {img}// Replace with your image URL
-        />)
+        !loading && (
+          <img
+            className="card-img"
+            alt="example"
+            src={img} // Replace with your image URL
+          />
+        )
       }
       actions={[
-        status === 'Completed' ?(
-            <span key="status">
-              <CheckCircleOutlined style={{ color: 'green' }} /> Completed
-            </span>
-          ) : (
-            <span key="status">
-              <HourglassOutlined style={{ color: 'orange' }} /> In Progress
-            </span>
-          ),
+        status === "Completed" ? (
+          <span key="status">
+            <CheckCircleOutlined style={{ color: "green" }} /> Completed
+          </span>
+        ) : (
+          <span key="status">
+            <HourglassOutlined style={{ color: "orange" }} /> In Progress
+          </span>
+        ),
 
-        <LinkOutlined 
-                  key="link" 
-                  onClick={() => handleCardClick(link)}
-                  style={{ cursor: link ? 'pointer' : 'not-allowed', opacity: link ? 1 : 0.5 }}  
+        <LinkOutlined
+          key="link"
+          onClick={() => handleCardClick(link)}
+          style={{
+            cursor: link ? "pointer" : "not-allowed",
+            opacity: link ? 1 : 0.5,
+          }}
 
-                   // Add click action
-                />
+          // Add click action
+        />,
       ]}
     >
       <Card.Meta
-        title={title}        
-        description={description}  // Replace with your description
+        title={title}
+        description={description} // Replace with your description
       />
     </Card>
   );
